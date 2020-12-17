ab677a1cc054fd6f567f9a35304e78cf5bd6e953
anogray <bbncr97@gmail.com>
2020-12-17 18:38:01 +0530

    added undo redo with sortby comparable

--- src/js/home.js
+++ src/js/home.js
@@ -6,31 +6,60 @@ class TodoHome extends React.Component {
     state = {
         arr: [],
         redoArr:[],
-        changed : false,
+        globalidx:0,
+        changed : 0,
+        savedval:{idx:0,val:0}
     };
-
-
+// 1 2 3 4 5 6 - 3 4 
+// [{val:val, id:idx4},{val:val, id:id3}]
 add = ()=>{
     console.log("adding btn fxn ")
-    if(this.state.arr.length==0)
-    this.setState({arr : [...this.state.arr,1]})
+    this.setState({globalidx:this.state.globalidx+1})
+    if(this.state.globalidx===0)
+    this.setState({arr : [...this.state.arr,{idx:0,val:1}]})
     else
-    this.setState({arr : [...this.state.arr,this.state.arr[this.state.arr.length-1]+1]})
+    {
+        let obj = {idx:this.state.globalidx, val:this.state.globalidx+1};
+        this.setState({arr : [...this.state.arr,obj]});
+    }
 }
 
-undo=()=>{
+undo=async()=>{
     console.log("undoing btn fxn ")
     const leftList = this.state.arr.length;
 
-    if(this.state.changed===true){
+    // if(this.state.changed===true){
         
-    }
+    // }
 
+    // else 
+
+    if(this.state.redoArr.length>0 && this.state.changed>0 )
+    { 
+        
+        this.setState({changed: this.state.changed-1});
+        // let updatearr = {...this.state.arr, ...this.state.redoArr[this.state.redoArr.length-1]}
+        // this.setState({updatearr})
+
+          await this.setState({arr:[...this.state.arr,this.state.redoArr[this.state.redoArr.length-1]]})
+        
+      
+
+       //this.state.arr.splice(this.state.savedval.idx,0,this.state.savedval)
+         this.setState({arr : [...this.state.arr]},()=>{
+            this.state.arr.sort((a,b)=>{
+                return a.idx-b.idx;
+            })
+        })
+        this.setState({redoArr:[...this.state.redoArr.filter(ar=>ar.idx!=this.state.redoArr[this.state.redoArr.length-1].idx)]})
+
+
+    }
     else if(leftList>0)
     {
         this.setState(({redoArr:[...this.state.redoArr, this.state.arr[this.state.arr.length-1]]}))
 
-        this.setState({arr:[...this.state.arr.filter(ar=>ar!=this.state.arr[this.state.arr.length-1])]})
+        this.setState({arr:this.state.arr.filter(ar=>ar.idx!=this.state.arr[this.state.arr.length-1].idx)})
  
         console.log("redoArr",this.state.redoArr)
 
@@ -39,26 +68,41 @@ undo=()=>{
     
 }
 
-redo=()=>{
+redo=async()=>{
     if(this.state.redoArr.length<=0)
     return;
 
-    this.setState({arr:[...this.state.arr,this.state.redoArr[this.state.redoArr.length-1]]})
-    this.setState({redoArr:[...this.state.redoArr.filter(ar=>ar!=this.state.redoArr[this.state.redoArr.length-1])]})
+    await this.setState({arr:[...this.state.arr,this.state.redoArr[this.state.redoArr.length-1]]})
+    this.setState({arr : [...this.state.arr]},()=>{
+            this.state.arr.sort((a,b)=>{
+                return a.idx-b.idx;
+            })
+        })
+    this.setState({redoArr:[...this.state.redoArr.filter(ar=>ar.idx!=this.state.redoArr[this.state.redoArr.length-1].idx)]})
 }
 
 
-remove=(val)=>{
+remove=async (arhandle)=>{
 
-    const index = this.state.arr.findIndex(ar => ar === val);
+    const index = this.state.arr.findIndex(ar => ar.val === arhandle.val);
     
     if(index!=-1)
-    {  console.log("inside remove fxn",val)
-    const savedval = this.state.arr[index];
-        this.setState({arr:[...this.state.arr.filter(ar=>ar!=val)]})
-        this.setState({redoArr:[...this.state.redoArr,savedval]})
-        this.setState({changed:true})
+    {  console.log("inside remove fxn",arhandle)
+    let savedobj = this.state.arr[index]
+    //the saveobj has structure of {idx:1 , val:1}
+    //-------------------------Option 1------------------------------
+    // this.state.savedval.idx=savedobj.idx;
+    // this.state.savedval.val=savedobj.val;
+    //--------------------------Option 2---------------------
+   await this.setState({savedval : {...this.state.savedval,...this.state.arr[index]}},
+    ()=>console.log("here callback ",this.state.savedval)
+    ) 
 
+        this.setState({arr:this.state.arr.filter(ar=>ar.val!=arhandle.val)})
+        this.setState({redoArr:[...this.state.redoArr,this.state.savedval]})
+        this.setState({changed: this.state.changed+1});
+        console.log(this.state.savedval)
+        
     }
 
 }
@@ -75,7 +119,7 @@ remove=(val)=>{
             <div className="list">
             {this.state.arr.map((ar) =>
                 (
-                    <li id={ar}>{ar}
+                    <li id={ar.idx}>{ar.val}
                     <button onClick={()=>this.remove(ar)}>Remove</button>
                     </li>
                 )
